1a624ec745ed875fa236f8456d34d2b568d92006
Jack Clarke <hello@idealpress.org>
2024-01-24 11:08:12 +0000

    Update Navigation.js

--- components/Navigation.js
+++ components/Navigation.js
@@ -8,15 +8,15 @@ const Navigation = () => {
 
   return (
     <div className="bg-neutral-50 dark:bg-black px-6 sticky top-0 z-20 transition-colors">
-      <div className="flex justify-between items-center font-serif border-b border-tf-red dark:border-tf-purple transition-colors px-2 pt-5 pb-4">
+      <div className="flex justify-between items-baseline sm:items-center font-serif border-b border-tf-red dark:border-tf-purple transition-colors px-2 pt-5 pb-4">
         <Link href="/about">
-            <a className="uppercase tracking-widest text-xs sm:text-base transition-colors text-tf-red dark:text-tf-purple focus:outline-dotted outline-1 outline-black outline-offset-2">
+            <a className="uppercase tracking-widest text-[0.6rem] sm:text-base transition-colors text-tf-red dark:text-tf-purple focus:outline-dotted outline-1 outline-black outline-offset-2">
               About
             </a>
           </Link>
-          <div className="md:pl-2 md:pr-12">
+          <div className="md:pl-2 md:pr-12 text-center">
             <Link href="/">
-              <a className="text-base md:text-lg lg:text-xl uppercase tracking-widest transition-colors text-tf-red dark:text-tf-purple focus:outline-dotted outline-1 outline-black outline-offset-0">
+              <a className="text-xs md:text-lg lg:text-xl uppercase tracking-widest transition-colors text-tf-red dark:text-tf-purple focus:outline-dotted outline-1 outline-black outline-offset-0">
                 Camberwell <span className="hidden sm:inline">College of Arts</span> Type Foundry
               </a>
             </Link>
@@ -27,7 +27,7 @@ const Navigation = () => {
             onClick={setDarkMode}
             className="cursor-pointer uppercase tracking-wider transition-colors w-6 text-lg text-tf-red dark:text-tf-purple sm:text-2xl leading-none"
           >
-            {darkMode ? <MoonSVG className="ml-1 -mt-0.5 w-3 h-4 sm:w-4 sm:h-5 fill-tf-purple" /> : <SunSVG className="w-5 h-5 sm:w-6 sm:h-6 -mt-0.5" />}
+            {darkMode ? <MoonSVG className="translate-y-0.5 ml-1 sm:-mt-0.5 w-3 h-4 sm:w-4 sm:h-5 fill-tf-purple" /> : <SunSVG className="w-4 h-4 sm:w-6 sm:h-6 translate-y-0.5 sm:-mt-0.5" />}
           </div>
         )}
       </div>
