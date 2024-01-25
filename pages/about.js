a048ecf110c1f2a1d6d5f1fa2050b063f53f0785
Jack Clarke <hello@idealpress.org>
2024-01-25 17:06:06 +0000

    Update about.js

--- pages/about.js
+++ pages/about.js
@@ -6,10 +6,10 @@ export default function About() {
         <p>Welcome to the Camberwell College of Arts Type Foundry. This website hosts fonts designed by BA (Hons) Graphic Design students, created during their first year of study on the course.</p>
         <p>The project asks students to design type in response to building or site in London. After taking part in workshops exploring the history and traditions of type design, the students conduct independent primary and contextual research to help develop their ideas into digital type files.</p>
         <p>You can read about each project in more detail by selecting an individual project page, test out the fonts using the type tester, or download .OTF or .TTF files for personal use.</p>
-        <p>The team would like to thank <a className="text-tf-red" href="https://www.elenapapassissa.it/">Elena Papassissa</a>, <a className="text-tf-red" href="https://www.europaeuropa.co.uk/">Rob Sollis</a>, <a className="text-tf-red" href="http://ya-rd.com/">Yenoju Yang</a>, <a className="text-tf-red" href="https://www.work-form.co.uk/about">Alex Hough</a> and <a className="text-tf-red" href="https://www.saf-ahm.com/">Safiya Ahmed</a> for all their work on this project.</p>
+        <p>The team would like to thank <a className="text-tf-red dark:text-tf-purple" href="https://www.elenapapassissa.it/">Elena Papassissa</a>, <a className="text-tf-red dark:text-tf-purple" href="https://www.europaeuropa.co.uk/">Rob Sollis</a>, <a className="text-tf-red dark:text-tf-purple" href="http://ya-rd.com/">Yenoju Yang</a>, <a className="text-tf-red dark:text-tf-purple" href="https://www.work-form.co.uk/about">Alex Hough</a> and <a className="text-tf-red dark:text-tf-purple" href="https://www.saf-ahm.com/">Safiya Ahmed</a> for all their work on this project.</p>
         <br/>
-        <p className="border-t border-1 -translate-y-6 border-black"/>
-        <p>If you are a graduate of the Graphic Design course at Camberwell, and would like to contribute your typeface to this foundry, <a className="text-tf-red" href="mailto:j.clarke@arts.ac.uk">get in touch with Jack via email: j.clarke@arts.ac.uk</a></p>
+        <p className="border-t border-1 -translate-y-6 border-black dark:border-white"/>
+        <p>If you are a graduate of the Graphic Design course at Camberwell, and would like to contribute your typeface to this foundry, <a className="text-tf-red dark:text-tf-purple" href="mailto:j.clarke@arts.ac.uk">get in touch with Jack via email: j.clarke@arts.ac.uk</a></p>
       </div>
     </div>
   )
